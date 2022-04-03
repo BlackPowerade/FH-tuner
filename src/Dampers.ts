c424ceaf1d0b55b4ad7377ff12e2e2f876a0c12e
BlackPowerade <esam3463@gmail.com>
2022-04-03 15:38:25 -0600

    Final this time

--- src/Dampers.ts
+++ src/Dampers.ts
@@ -7,8 +7,8 @@ export function Dampers(mass: number, HZ: number, critdamp: number, ratio: numbe
 {
     // Spring rate must be in N/m, weight in kg.
     // critdamp defaults to 0.8, go higher for overdamping, lower for under damping. 
-    let base = (2 * HZ * (mass / 2) * critdamp) / 100
-    let bump = base / (1 / ratio)
+    let base = ((2 * HZ * (mass / 2) * critdamp) / 100) * 2
+    let bump = base / (1 + (1 / ratio))
     let rebound = base - bump
 
     return {'bump': bump, 'rebound': rebound}
